86fa46be11661252581907f9d842bbdb84330ac5
Erdvik97 <magnuserdvik@hotmail.com>
2020-06-10 12:23:19 +0200

    added cho han game and did_win helper method

--- games_starting/games.py
+++ games_starting/games.py
@@ -1,18 +1,21 @@
 import random
+from time import sleep
 
 def main():
     #Call your game of chance functions here
 
-    coinflip("Tails", 20)
-    print(money)
+    # coinflip("Tails", 20)
+    # print(money)
+
+    cho_han("Odd", 10)
 
 
 
 money = 100
 
 #Write your game of chance functions here
-avalible_guesses = ["Heads", "Tails"]
 def coinflip(guess, amount):
+    avalible_guesses = ["Heads", "Tails"]
     if guess in avalible_guesses: 
         global money
         if amount <= money:
@@ -22,19 +25,60 @@ def coinflip(guess, amount):
                 coin = "Heads"
             elif num == 2:
                 coin = "Tails"
-
             if guess == coin:
-                amount_won = amount * 2
-                money += amount_won
-                print('You won! New balance is {money}.'.format(money=money))
+                did_win(True, amount, 2)           
             else:
-                money -= amount
-                print('You lost.. New balance is {money}.'.format(money=money))
+                did_win(False, amount)     
         else:
             print('Not enough money to play.')
     else:
         print('Not an avalible guess. Here are the avalible guesses:')
         print(avalible_guesses)
+
+def cho_han(guess, amount):
+    avalible_guesses = ["Odd", "Even"]
+    if guess in avalible_guesses:
+        global money
+        if amount <= money:
+            print('Rolling dices...')
+            sleep(2)
+            dice_roll_1 = random.randint(1, 6)
+            print('First roll: {0}'.format(dice_roll_1))
+            sleep(2)
+            dice_roll_2 = random.randint(1, 6)
+            print('Second roll: {0}'.format(dice_roll_2))
+            sleep(1)
+            score = dice_roll_1 + dice_roll_2
+            print('Score: {0}'.format(score))
+            sleep(1)
+            if score % 2 == 0: #even number
+                if guess == "Even":
+                    did_win(True, amount, 2)
+                else:
+                    did_win(False, amount)
+            else: #odd number
+                if guess == "Odd":
+                    did_win(True, amount, 2)
+                else:
+                    did_win(False, amount)
+                
+def did_win(won, amount, multiplier=2):
+    global money
+    if won == True:
+        money += amount * multiplier
+        print('You won! New balance is {money}.'.format(money=money))
+    elif won == False:
+        money -= amount
+        print('You lost.. New balance is {money}.'.format(money=money))
+    else:
+        print('Error in won_or_loss function')          
+                
+
+
+
+
+
+
 
 
 
